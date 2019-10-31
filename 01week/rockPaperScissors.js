fe577fa74739c2056bdc332b1de472b81e2643a0
anthayes96 <ant.hayes96@gmail.com>
2019-10-30 20:11:08 -0500

    finished

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,13 +9,31 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
+  hand1 = hand1.toLowerCase().trim();
+  hand2 = hand2.toLowerCase().trim();
 
   // Write code here
-  if (hand === hand2) {
-  return "it's a tie!";
-  } else if (hand1 === 'rock' && hand2 === 'scissors') {
-    return "hand one wins!"
-  }
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  } else if 
+    (hand1 === 'rock' && hand2 === 'scissors') {
+    return "Hand one wins!"
+    } else if
+    (hand1 === 'paper' && hand2 === 'rock') {
+      return "Hand one wins!"
+    } else if 
+    (hand1 === 'scissors' && hand2 === 'paper') {
+      return "Hand one wins!"
+    } else if 
+    (hand2 === 'rock' && hand1 === 'scissors') {
+    return "Hand two wins!"
+    } else if
+    (hand2 === 'paper' && hand1 === 'rock') {
+      return "Hand two wins!"
+    } else if  
+    (hand2 === 'scissors' && hand1 === 'paper') {
+      return "Hand two wins!"
+    }
 
 }
 
